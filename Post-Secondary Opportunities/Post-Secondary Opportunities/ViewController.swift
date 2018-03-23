5eda88f39777ff8a062e42b92ce3a8525a877fab
Jason Guo <jasonyzguo@gmail.com>
2018-03-23 10:24:34 -0400

    Finish simple app for post-secondary

--- Post-Secondary Opportunities/Post-Secondary Opportunities/ViewController.swift
+++ Post-Secondary Opportunities/Post-Secondary Opportunities/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var overwatch_link = "https://www.reddit.com/r/Overwatch/comments/815v6k/question_for_a_high_school_computer_science/dv16ukb/"
-    @IBAction func overwatchReddit(_ sender: UIButton) {
-        UIApplication.shared.open(URL(string : overwatch_link)!, options: [:], completionHandler: { (status) in
+    
+
+    @IBOutlet var linkButtons: [UIButton]!
+    
+    var links = ["https://goo.gl/9xveRv", "https://goo.gl/7p3Xo5", "https://goo.gl/bg7Ei8", "https://goo.gl/xq1E7c", "https://goo.gl/Qxpj4X"]
+    //var overwatch_link = "https://www.reddit.com/r/Overwatch/comments/815v6k/question_for_a_high_school_computer_science/dv16ukb/"
+    
+    @IBAction func openLink(_ sender: UIButton) {
+        if let linkNumber = linkButtons.index(of: sender) {
+            UIApplication.shared.open(URL(string : links[linkNumber])!, options: [:], completionHandler: { (status) in
+                
+            })
             
-        })
-    }
+        } else {
+            print("The button pressed is not in linkButtons")
+        
+        }
+        
+        }
 }
 
