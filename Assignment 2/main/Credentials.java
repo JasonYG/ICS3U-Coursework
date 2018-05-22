4f21f8a8d399e5c147e209541d0f16ce65e37348
Guo <568896@pdsb.org>
2018-05-22 08:45:33 -0400

    Create front-end welcome screen

--- Assignment 2/main/Credentials.java
+++ Assignment 2/main/Credentials.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import java.awt.*;
 /**
  * This class is used to register new users.
  *
@@ -9,13 +10,24 @@ import javax.swing.JOptionPane;
  */
 public class Credentials {
 
-   /**
-    * Registers the user's credentials
-    */
+  /**
+   * Registers the user's credentials
+   */
   public static void register() {
     String username = JOptionPane.showInputDialog("Enter a username:");
-    if (username == null) {
-    System.out.println("Please enter a username");
+    if (username == null || username.length() < 6) {
+      JOptionPane.showMessageDialog(null, "Please enter a valid username.");
+      return;
     }
+    //if Credentials.authenticate(username) {
+    //}
+    String password = JOptionPane.showInputDialog("Enter a password:");
+    if (password == null || password.length() < 6) {
+      JOptionPane.showMessageDialog(null, "Please enter a valid password.");
+    }
+  }
+  public static boolean authenticate(String username) {
+    
+    return true;
   }
 }
--- Assignment 2/main/main.pde
+++ Assignment 2/main/main.pde
@@ -1,7 +1,10 @@
 int screen = 0;
+PFont font;
 void settings() {
+  size(600, 600);
 }
 void setup() {
+  font = createFont("IBMPlexSans-Medium.ttf", 30);
 }
 void draw() {
   switch (screen) {
@@ -11,5 +14,30 @@ void draw() {
   }
 }
 void registrationScreen() {
-  Credentials.register();
+  background(255);
+
+  fill(0);
+  textAlign(CENTER);
+  textFont(font);
+  text("Welcome to Assignment 2", width/2, 200);
+
+  fill(255);
+  rect(0, 450, width/2, 150);
+  fill(0);
+  text("Login", width/4, 525);
+
+  fill(255);
+  rect(width/2, 450, width/2, 150);
+  fill(0);
+  text("Register", 3*width/4, 525);
+}
+void mousePressed() {
+  if (screen == 0) {
+    if (mouseX > 0 && mouseX < width/2 && mouseY > 450 && mouseY < height) {
+      Credentials.login();
+    }
+    if (mouseX > width/2 && mouseX < width && mouseY > 450 && mouseY < height) {
+      Credentials.register();
+    }
+  }
 }
