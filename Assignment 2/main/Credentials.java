64d0c58840094183fad1cf57373dfbbacd1e6e08
Guo <568896@pdsb.org>
2018-05-23 10:35:44 -0400

    Add username registration

--- Assignment 2/main/Credentials.java
+++ Assignment 2/main/Credentials.java
@@ -1,5 +1,8 @@
 import javax.swing.JOptionPane;
 import java.awt.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 /**
  * This class is used to register new users.
  *
@@ -19,16 +22,44 @@ public class Credentials {
       JOptionPane.showMessageDialog(null, "Please enter a valid username.");
       return;
     }
+    if (!authenticate(username)) {
+      JOptionPane.showMessageDialog(null, "The username already exists.");
+      return;
+    }
     String password = JOptionPane.showInputDialog("Enter a password:");
     if (password == null || password.length() < 6 || password.length() > 15) {
       JOptionPane.showMessageDialog(null, "Please enter a valid password.");
+      return;
+    }
+    try {
+      BufferedWriter usernameLists = new BufferedWriter(new FileWriter("G:\\Documents\\ICS 3U\\Assignment 2\\main\\data\\username.txt", true));
+      usernameLists.write(username);
+      usernameLists.write("\n")
+      usernameLists.close();
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
     }
   }
+
   /**
    * Javadoc
    */
-  private static boolean authenticate(String username, String password) {
-    
+  private static boolean authenticate(String username) {
+    Scanner usernames;
+    try {
+      File usernameLists = new File("G:\\Documents\\ICS 3U\\Assignment 2\\main\\data\\username.txt");
+      usernames = new Scanner(usernameLists);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+    while (usernames.hasNextLine()) {
+      if (username.equals(usernames.nextLine())) {
+        return false;
+      }
+    }
     return true;
   }
   /**
@@ -37,7 +68,6 @@ public class Credentials {
   public static void login() {
     String username = JOptionPane.showInputDialog("Enter your username");
     String password = JOptionPane.showInputDialog("Enter your password");
-    authenticate(username, password);
-    
+    //authenticate(username, password);
   }
 }
