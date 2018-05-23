d2d7f4d2ba8d60fb4ca5e6a57ad273404c21e980
Jason Guo <jasonyzguo@gmail.com>
2018-05-23 08:10:49 -0400

    Finish front-end

--- Assignment 2/main/Credentials.java
+++ Assignment 2/main/Credentials.java
@@ -15,19 +15,29 @@ public class Credentials {
    */
   public static void register() {
     String username = JOptionPane.showInputDialog("Enter a username:");
-    if (username == null || username.length() < 6) {
+    if (username == null || username.length() < 6 || username.length() > 15) {
       JOptionPane.showMessageDialog(null, "Please enter a valid username.");
       return;
     }
-    //if Credentials.authenticate(username) {
-    //}
     String password = JOptionPane.showInputDialog("Enter a password:");
-    if (password == null || password.length() < 6) {
+    if (password == null || password.length() < 6 || password.length() > 15) {
       JOptionPane.showMessageDialog(null, "Please enter a valid password.");
     }
   }
-  public static boolean authenticate(String username) {
+  /**
+   * Javadoc
+   */
+  private static boolean authenticate(String username, String password) {
     
     return true;
+  }
+  /**
+   * JAVADOC
+   */
+  public static void login() {
+    String username = JOptionPane.showInputDialog("Enter your username");
+    String password = JOptionPane.showInputDialog("Enter your password");
+    authenticate(username, password);
+    
   }
 }
